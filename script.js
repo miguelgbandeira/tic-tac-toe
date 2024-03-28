1344d846e3e415fbb3c78eba1b588872d5a6ef2b
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-03-28 12:48:21 +0000

    Finish tic tac toe game logic to display in console

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-function tictactoe() {
-  const playerOne = createPlayer("Player1", "X");
-  const playerTwo = createPlayer("Player2", "O");
-  const board = createBoard();
-}
-
 function createBoard() {
   const board = [
     ["", "", ""],
@@ -18,14 +12,14 @@ function createBoard() {
     }
   };
 
-  return { displayBoard };
+  return { board, displayBoard };
 }
 
 function createPlayer(name, mark) {
   return { name, mark };
 }
 
-function playMove(mark, row, col, board) {
+function playMove(board, row, col, mark) {
   if (board[row][col] == "") {
     board[row][col] = mark;
     return true;
@@ -33,7 +27,7 @@ function playMove(mark, row, col, board) {
   return false;
 }
 
-function isWinner(mark, board) {
+function isWinner(board, mark) {
   for (let i = 0; i < 3; i++) {
     if (
       (board[i][0] == mark && board[i][1] == mark && board[i][2] == mark) ||
@@ -62,5 +56,35 @@ function isBoardFull(board) {
   return true;
 }
 
-const board = createBoard();
-board.displayBoard();
+function tictactoe() {
+  const playerOne = createPlayer("Player1", "X");
+  const playerTwo = createPlayer("Player2", "O");
+  const board = createBoard();
+  board.displayBoard();
+  let currentPlayer = playerOne;
+  while (true) {
+    console.log(`${currentPlayer.name}'s turn:`);
+    let row = parseInt(prompt("Enter row (0-2):"));
+    let col = parseInt(prompt("Enter column (0-2):"));
+    console.log({ row, col });
+    if (row >= 0 && row <= 2 && col >= 0 && col <= 2) {
+      if (playMove(board.board, row, col, currentPlayer.mark)) {
+        board.displayBoard();
+        if (isWinner(board.board, currentPlayer.mark)) {
+          console.log(`Player ${currentPlayer.name} wins!`);
+          break;
+        } else if (isBoardFull(board.board)) {
+          console.log("It's a tie!");
+          break;
+        }
+        currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne; // Switch player
+      } else {
+        console.log("Invalid move. Try again.");
+      }
+    } else {
+      console.log("Invalid input. Row and column must be between 0 and 2.");
+    }
+  }
+}
+
+tictactoe();
